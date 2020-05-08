a9f0ad55235bb3363de5b4fee715a809bcfea6d2
srgoti <fl.valentin1904@gmail.com>
2020-05-08 12:34:55 +0200

    Removed some warnings

--- app/src/main/java/org/flval/discordwebview/SettingsActivity.java
+++ app/src/main/java/org/flval/discordwebview/SettingsActivity.java
@@ -1,17 +1,11 @@
 package org.flval.discordwebview;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.preference.MultiSelectListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceManager;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
@@ -24,14 +18,12 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_settings);
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
 
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settingslayout, new SettingsFragment())
-                .addToBackStack(null)
                 .commit();
 
 
--- app/src/main/java/org/flval/discordwebview/SettingsFragment.java
+++ app/src/main/java/org/flval/discordwebview/SettingsFragment.java
@@ -1,25 +1,12 @@
 package org.flval.discordwebview;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferencescreen, rootKey);
-        PreferenceManager pm = getPreferenceManager();
-        Preference css = (Preference) findPreference("customcss");
     }
 
 }
